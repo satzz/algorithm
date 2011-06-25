51b7e2a1a99ee4b7a18bc2605f7b5a1135ac110a
satzz <satzz21@gmail.com>
2011-06-26 07:56:57 +0900

    AVL

---
+++ lib/AVL.pm
@@ -0,0 +1,68 @@
+package AVL;
+use strict;
+use warnings;
+use base qw/BSTree/;
+
+__PACKAGE__->mk_accessors qw/_height/;
+
+sub height { $_[0]->_height }
+
+sub new {
+    my $class = shift;
+    my $self = $class->SUPER::new(@_);
+    $self->_height(defined $self->val ? 1 : 0);
+    bless $self, $class;
+    return $self;
+    
+}
+
+sub add_one {
+    my $self = shift;
+    $self->SUPER::add_one(shift);
+    my $leaf = $self->last_modified;
+    bless $leaf, __PACKAGE__;
+    my $target = $leaf;
+    while ($target) {
+        my $max = 1;
+        for my $lr qw/left right/ {
+            my $child = $target->$lr or next;
+            $max < $child->height + 1 and $max = $child->height + 1;
+        }
+        $target->_height($max);
+        $target = $target->parent;
+    }
+    $self;
+}
+
+sub is_valid {
+}
+
+sub rotate {
+    my ($self, $direction)  = @_;
+    my $opp = __PACKAGE__->opposite($direction) or return $self;
+
+    my $lr = $self->lr;
+    if (my $parent = $self->parent) {
+        my $child = $self->$opp;
+        $parent->$lr($child);
+        if ($child) {
+            $child->$direction($self);
+            $child->parent_weaken($parent);
+            $self->parent_weaken($child);
+
+            my $grand_child = $child->$direction;
+            $self->$opp($grand_child);
+            $grand_child and $grand_child->parent($self);
+        }
+    }
+    return $self;
+}
+
+sub opposite {
+    my ($class, $direction) = @_;
+    $direction eq 'left' ? `right` :
+    $direction eq 'right' ? `left` :
+    undef;
+}
+
+1;
---
+++ t/02_avl.t
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+use Test::More qw/no_plan/;
+use JSON::Syck;
+use YAML;
+use AVL;
+use Data::Dumper;
+use Perl6::Say;
+use Test::LeakTrace;
+
+BEGIN {use_ok 'AVL'};
+
+my $tree = AVL->new;
+
+is $tree->height, 0;
+$tree->init(11);
+is $tree->last_modified, $tree;
+is $tree->last_modified->height, 1;
+
+
+# is $tree->height, 1;
+# die;
+
+$tree->init(11,8,9,10,6,7,4,5,3,14,15,12,13);
+is $tree->height, 5;
+is $tree->search(8)->height, 4;
+is $tree->search(6)->height, 3;
+is $tree->search(4)->height, 2;
+is $tree->search(3)->height, 1;
+is $tree->search(10)->height, 1;
+is $tree->search(9)->height, 2;
+
+# warn Dumper $tree;
+
+
+
